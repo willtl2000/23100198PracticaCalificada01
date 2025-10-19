d5ba65530c05baca07890ea781276530b7a8f075
Wilber Ticona <wilber.ticona@outlook.com>
2025-10-18 20:15:32 -0500

    feat: Add Firebase Auth and Firestore dependencies
    Adds the necessary dependencies for Firebase Authentication and Firebase Firestore.
    
    This includes:
    - Adding the libraries to the `libs.versions.toml` file.
    - Implementing the new libraries in the `app/build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.google.firebase.auth)
+    implementation(libs.google.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
